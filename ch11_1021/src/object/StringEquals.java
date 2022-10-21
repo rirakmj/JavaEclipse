4e92ca2cc97f2b0883351854091c5371824f5f71
admin <admin@DESKTOP-7NNKG9M.mshome.net>
2022-10-21 14:41:16 +0900

    update

--- ch11_1021/src/object/StringEquals.java
+++ ch11_1021/src/object/StringEquals.java
@@ -7,6 +7,8 @@ public class StringEquals {
 		String str1 = new String("abc");
 		String str2 = new String("abc");
 		
+		System.out.println("str1의 참조형 변수의 메모리 주소값 : " + str1);
+		System.out.println("str2의 참조형 변수의 메모리 주소값 : " + str2);
 		System.out.println(str1 == str2);  // 두 스트링 인스턴스의 주소 값은 다름
 		System.out.println(str1.equals(str2)); // String 클래스의 equals 메소드가 재정의 됨
 		
